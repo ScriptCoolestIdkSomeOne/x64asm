e62bbeef29bbed5a169afa049c455f3235c89780
Dadatel <velikiydad@gmail.com>
2026-08-24 03:08:48 +0300

    shittyshitshit, well this shit doesn't have anything cool anyways
    I FUCKING FORGOT ABOUT THE OTHER BRANCH

---
+++ parse.h
@@ -0,0 +1,12 @@
+//im too fakn tired to bloat x64_asm.h for some petty instructions
+//so here will be just P_parse
+//also i got an idea to make a file that like checks if all the shit is correct then just make it go through, like a lil secutity(for dumbasses and for me after a year
+//because i am a dumbass)
+//oh and i will try to SOMEHOW OPTIMIZE NoCRT_printf because i still have a suspicion that i didn't optimized everything yet(yes optimizations flags but
+//cmon it's boring, my shitty idea is that you include the header, see the shit that's it's good, use it afterwards, and well because im too lazy to link libs
+//and ye optimization flags are just boring optimizations, but without em NoCRT shit doesn't works as intented, well NODEFAULTLIB just fucking dies and whines about
+//some pitty shit like "i want -GS, you got chkstk wrong, my shitty engine can't handle this, i can't handle that, i need additional flags to work without
+//my bloat, i need a custom entry point" like cmon, it's like his lil toys that he can't do shit without them and you need to buy a boy another toy)
+//oh and ye i maybe i will do some other shit along with this shitty project
+//oh and i want some big ass license like some corporations got, looks cool and official but also lame and corporate but still
+// oh and ye for someone who readed till here, im not the one person, i mean like i got a team not a lot of other me's
